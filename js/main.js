d865e8e2c2f4715f3903d63ba1cdca50ea43b51f
Filbert Fuvian <ffuvian@gmail.com>
2024-10-21 16:14:45 +0700

    huge progress

--- js/main.js
+++ js/main.js
@@ -3,40 +3,73 @@ function navigateToCourse() {
     const coursesearch = document.getElementById("course");
     const selectedCourse = coursesearch.value;
 
-    // Redirect to the corresponding course page
     switch (selectedCourse) {
         case "Matematika":
-            window.location.href = "pages/matematika.html";
+            window.location.href = "courses.html";
             break;
         case "Fisika":
-            window.location.href = "pages/fisika.html";
+            window.location.href = "courses.html#fisika";
             break;
         case "Kimia":
-            window.location.href = "pages/kimia.html";
+            window.location.href = "courses.html#kimia";
             break;
         case "Berpikir Komputational":
-            window.location.href = "pages/pengkom.html";
+            window.location.href = "courses.html#pengkom";
             break;
         case "Literasi Digital dan AI":
-            window.location.href = "pages/litdig.html";
+            window.location.href = "courses.html#litdig";
             break;
         case "Olahraga":
-            window.location.href = "pages/olahraga.html";
+            window.location.href = "courses.html#olahraga";
             break;
         case "Pancasila":
-            window.location.href = "pages/pancasila.html";
+            window.location.href = "courses.html#pancasila";
             break;
         case "Bahasa Inggris":
-            window.location.href = "pages/english.html";
+            window.location.href = "courses.html#english";
             break;
         case "Bahasa Indonesia":
-            window.location.href = "pages/bahasa.html";
+            window.location.href = "courses.html#bahasa";
             break;
         default:
-            alert("Please select a valid course from the list." + selectedCourse + coursesearch);
+            alert("Please select a valid course from the list.");
             return false;
     }
 
-    // Prevent form submission
     return false;
 }
+
+// function buttonToCourse { 
+//     const courseButtton = document.getElementById("courseButton");
+//     const selectedCourse = courseButtton.value;
+//     switch (selectedCourse) {
+//         case "Matematika":
+//             window.location.href = "pages/matematika.html";
+//             break;
+//         case "Fisika":
+//             window.location.href = "pages/fisika.html";
+//             break;
+//         case "Kimia":
+//             window.location.href = "pages/kimia.html";
+//             break;
+//         case "Berpikir Komputational":
+//             window.location.href = "pages/pengkom.html";
+//             break;
+//         case "Literasi Digital dan AI":
+//             window.location.href = "pages/litdig.html";
+//             break;
+//         case "Olahraga":
+//             window.location.href = "pages/olahraga.html";
+//             break;
+//         case "Pancasila":
+//             window.location.href = "pages/pancasila.html";
+//             break;
+//         case "Bahasa Inggris":
+//             window.location.href = "pages/english.html";
+//             break;
+//         case "Bahasa Indonesia":
+//             window.location.href = "pages/bahasa.html";
+//             break;
+//     }   
+// }
+// // 
